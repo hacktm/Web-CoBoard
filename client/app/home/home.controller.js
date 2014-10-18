6deec73bd040b3aafbc000615dca7c1617be2a22
Roland SEBESTYEN <sebestyenroland21@gmail.com>
2014-10-18 22:13:59 +0300

    add angularJS join page

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('coboard')
 
-.controller('HomepageController', function( $scope, socket){
+.controller('HomepageController', function( $scope, socket, $location){
 
 	$scope.name = '';
 
@@ -12,6 +12,7 @@ angular.module('coboard')
 
 	$scope.startRoom = function(){
 		socket.emit('room.create', {name: $scope.name, room: null});
+		$location.path('/join')
 	}
 
 });
