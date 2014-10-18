dff155e28f4ce8e515fcc702b40679e65baa55b5
Albulescu Cosmin <cosmin@albulescu.ro>
2014-10-18 22:13:59 +0300

    Fix

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,10 +5,25 @@ angular.module('coboard')
 .controller('BoardController', function( $scope ){
 
 	$scope.init = function() {
-
 	};
 	
 	$scope.logout = function() {
 		alert('Logout');
+	};
+})
+
+.controller('chat', function($scope,socket){
+
+	$scope.init = function() {
+	};
+
+	$scope.message = '';
+
+	$scope.users = [{ id : 1, name : 'Ionica'}];
+
+	$scope.messages = [{ text : 'test', user: 'user' }];
+
+	$scope.send = function() {
+		socket.emit('message', this.message);
 	};
 });
